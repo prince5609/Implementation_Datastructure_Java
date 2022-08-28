1fea457d7605bddc3b9cacf1215b885fda6bc7d9
PRINCE <prince5609>
2022-08-28 07:55:51 +0530

    Quick_Sort

--- src/com/DataStructure/Bubble_Sort.java
+++ src/com/DataStructure/Bubble_Sort.java
@@ -1,17 +1,25 @@
 package com.DataStructure;
 
 public class Bubble_Sort {
-    public static void main(String[] args) {
-        int [] a = {9,3,7,25,12,69,45};
-        for (int i = 0; i < a.length - 1; i++){
-            if(a[i] > a[i+1]){
-                int temp = a[i];
-                a[i] = a[i+ 1];
-                a[i+1] = temp;
+    public static int[] bubble_sort(int []arr){
+        for (int i = 0; i < arr.length -1 ; i++){
+            if (arr[i] > arr[i + 1]){
+                swap(arr, i, i+ 1);
             }
         }
-        for (int i = 0; i < a.length; i++){
-            System.out.print(a[i] + " ");
+        return arr;
+    }
+    public static void swap(int[]arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public static void main(String [] args){
+        int [] arr = {9, 3, 7, 25, 12, 69, 45};
+        bubble_sort(arr);
+        for (int i = 0; i < arr.length; i++){
+            System.out.print( arr[i] + " ");
         }
     }
 }
