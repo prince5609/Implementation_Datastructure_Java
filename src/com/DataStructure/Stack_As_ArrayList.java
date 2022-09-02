faba03cdb2deabc5a6f11b9e09dd3d2c19a907d1
PRINCE <prince5609>
2022-09-02 12:29:23 +0530

    Stack_As_ArrayList

---
+++ src/com/DataStructure/Stack_As_ArrayList.java
@@ -0,0 +1,42 @@
+package com.DataStructure;
+
+import java.util.ArrayList;
+
+public class Stack_As_ArrayList {
+     static class Stack {
+        static ArrayList<Integer> list = new ArrayList<Integer>();
+
+        public static boolean isEmpty() {
+            return list.size() == 0;
+        }
+
+        public static void push(int data) {
+            list.add(data);
+        }
+
+        public static void pop() {
+            list.remove(list.size() - 1);
+        }
+
+        public static int peek(){
+            if (isEmpty()){
+                return -1;
+            }
+            return list.get(list.size()- 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+
+        while (s.isEmpty() != true){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
