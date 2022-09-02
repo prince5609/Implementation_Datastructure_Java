00e5f5c59f06c8d6e8ebc6b802226576d9f98510
PRINCE <prince5609>
2022-09-02 11:45:09 +0530

    Array_List

---
+++ src/com/DataStructure/Array_List.java
@@ -0,0 +1,53 @@
+package com.DataStructure;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Array_List {
+    public static void main (String[]args){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        // ADD
+
+        list.add(5);  // Here 5 is value
+        list.add(2);
+        list.add(4);
+
+        System.out.println(list);
+
+        // GET
+
+        int element = list.get(1);  // Here 1 is index
+        System.out.println(element);
+
+        // ADD IN BETWEEN
+
+        list.add(1, 1);
+        System.out.println(list);
+
+        // CHANGE ELEMENT VALUE
+
+        list.set(3, 3);   // Here first(3) is index and second(3) is value.
+        System.out.println(list);
+
+        // REMOVE
+
+        list.remove(3);  // Here 3 is index.
+        System.out.println(list);
+
+        // SIZE
+
+        System.out.println("Size of list is " + list.size());
+
+        // PRINT BY LOOPS
+
+        for (int i = 0; i < list.size();i++){
+            System.out.print(list.get(i));
+        }
+        System.out.println();
+
+        // SORTING
+
+        Collections.sort(list);
+        System.out.println(list);
+
+    }
+}
