112fc85ebba46d3dd212d95f8acb96ccb1126f63
PRINCE <prince5609>
2022-09-04 15:33:26 +0530

    Quick_Sort

--- src/com/DataStructure/Quick_Sort.java
+++ src/com/DataStructure/Quick_Sort.java
@@ -1,14 +1,14 @@
 package com.DataStructure;
 
 public class Quick_Sort{
-    public static void swap(int arr[], int a, int b){
+    public static void swap(int[] arr, int a, int b){
         if (a != b){
             int temp = arr[a];
             arr[a] = arr[b];
             arr[b] = temp;
         }
     }
-    public static int partition(int arr[], int start, int end){
+    public static int partition(int[] arr, int start, int end){
         int pivot_index = start;
         int pivot = arr[pivot_index];
         while(start <  end){
@@ -25,19 +25,19 @@ public class Quick_Sort{
         swap(arr, pivot_index, end);
         return end;
     }
-    public static int[] quickSort(int[] arr, int start, int end){
+    public static void quickSort(int[] arr, int start, int end){
         if (start < end){
             int pi = partition(arr, start, end);
             quickSort(arr, start, pi - 1);
             quickSort(arr, pi + 1, end);
         }
-        return arr;
     }
     public static void main(String [] args){
         int [] arr = {11, 9, 7, 29, 2, 28};
         quickSort(arr,0,arr.length - 1);
-        for (int i = 0 ;i < arr.length;i++){
-            System.out.print(arr[i] + " ");
+
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 }
