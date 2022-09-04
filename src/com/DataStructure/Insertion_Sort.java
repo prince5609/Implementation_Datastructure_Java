f52c06ddbd4313cf73e934ed3a49d8e1914c2373
PRINCE <prince5609>
2022-09-04 15:32:11 +0530

    Insertion_Sort

--- src/com/DataStructure/Insertion_Sort.java
+++ src/com/DataStructure/Insertion_Sort.java
@@ -1,7 +1,7 @@
 package com.DataStructure;
 
 public class Insertion_Sort {
-    public static int[] insertionSort(int[] arr){
+    public static void insertionSort(int[] arr){
         for (int i = 1; i < arr.length;i++){
             int j = i -1;
             int anchor = arr[i];
@@ -11,13 +11,12 @@ public class Insertion_Sort {
             }
             arr[j + 1] = anchor;
         }
-        return arr;
     }
     public static void main(String [] args){
         int [] arr = {21, 29, 38, 17, 4, 25, 11, 32, 9};
         insertionSort(arr);
-        for (int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 }
